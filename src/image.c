4113816b3047b2989a827a16f59ca3a536a2f885
EsGeh <samuel@esgeh.net>
2025-04-15 00:38:09 +0200

    image: save all rgb formats (except deprecated ones)

--- src/image.c
+++ src/image.c
@@ -1,6 +1,7 @@
 #include "image.h"
 #include "output.h"
 #include "global.h"
+#include <linux/videodev2.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -14,26 +15,263 @@
 #define GET_CHANNEL( VALUE, SIZE, POS ) \
 	(((VALUE) & (((1 << (SIZE))-1) << (POS))) >> (POS))
 
+#define GET_CHANNEL_SIMPLE(FORMAT,CHANNEL,PIXEL) \
+	GET_CHANNEL(*(PIXEL+CAT2(FORMAT,CHANNEL,_BYTE)),CAT2(FORMAT,CHANNEL,_SIZE),CAT2(FORMAT,CHANNEL,_POS))
 
-#define RGB332_TYPE byte_t
+/*****************
+ * RGB332
+ *****************/
+
+#define RGB332_SIZE 1
+#define RGB332_RED_BYTE 0
 #define RGB332_RED_SIZE 3
 #define RGB332_RED_POS 5
+
+#define RGB332_GREEN_BYTE 0
 #define RGB332_GREEN_SIZE 3
 #define RGB332_GREEN_POS 2
+
+#define RGB332_BLUE_BYTE 0
 #define RGB332_BLUE_SIZE 2
 #define RGB332_BLUE_POS 0
 
+#define RGB332_GET_RED(PIXEL) GET_CHANNEL_SIMPLE(RGB332,_RED,PIXEL)
+#define RGB332_GET_GREEN(PIXEL) GET_CHANNEL_SIMPLE(RGB332,_GREEN,PIXEL)
+#define RGB332_GET_BLUE(PIXEL) GET_CHANNEL_SIMPLE(RGB332,_BLUE,PIXEL)
 
-#define RGB332_GET_RED(BYTE) \
-	GET_CHANNEL( BYTE, 3, 5 )
+/*****************
+ * ARGB444 or XRGB444 
+ *****************/
 
-#define RGB332_GET_GREEN(BYTE) \
-	GET_CHANNEL( BYTE, 3, 2 )
+#define XRGB444_SIZE 2
 
-#define RGB332_GET_BLUE(BYTE) \
-	GET_CHANNEL( BYTE, 2, 0 )
+#define XRGB444_RED_BYTE 1
+#define XRGB444_RED_SIZE 4
+#define XRGB444_RED_POS 0
 
-#define CAT( a, ...) a ## __VA_ARGS__
+#define XRGB444_GREEN_BYTE 0
+#define XRGB444_GREEN_SIZE 4
+#define XRGB444_GREEN_POS 4
+
+#define XRGB444_BLUE_BYTE 0
+#define XRGB444_BLUE_SIZE 4
+#define XRGB444_BLUE_POS 0
+
+#define XRGB444_GET_RED(PIXEL) GET_CHANNEL_SIMPLE(XRGB444,_RED,PIXEL)
+#define XRGB444_GET_GREEN(PIXEL) GET_CHANNEL_SIMPLE(XRGB444,_GREEN,PIXEL)
+#define XRGB444_GET_BLUE(PIXEL) GET_CHANNEL_SIMPLE(XRGB444,_BLUE,PIXEL)
+
+/*****************
+ * ARGB555 or XRGB555
+ *****************/
+
+#define XRGB555_SIZE 2
+
+#define XRGB555_RED_BYTE 1
+#define XRGB555_RED_SIZE 5
+#define XRGB555_RED_POS 2
+
+#define XRGB555_GREEN_SIZE 5
+
+#define XRGB555_BLUE_BYTE 0
+#define XRGB555_BLUE_SIZE 5
+#define XRGB555_BLUE_POS 0
+
+#define XRGB555_GET_RED(PIXEL) GET_CHANNEL_SIMPLE(XRGB555,_RED,PIXEL)
+#define XRGB555_GET_GREEN(PIXEL) \
+	GET_CHANNEL(PIXEL[0],3,5) \
+	| (GET_CHANNEL(PIXEL[1],2,0) << 3)
+#define XRGB555_GET_BLUE(PIXEL) GET_CHANNEL_SIMPLE(XRGB555,_BLUE,PIXEL)
+
+/*****************
+ * RGB565 
+ *****************/
+
+#define RGB565_SIZE 2
+
+#define RGB565_RED_BYTE 1
+#define RGB565_RED_SIZE 5
+#define RGB565_RED_POS 3
+
+#define RGB565_GREEN_SIZE 6
+
+#define RGB565_BLUE_BYTE 0
+#define RGB565_BLUE_SIZE 5
+#define RGB565_BLUE_POS 0
+
+#define RGB565_GET_RED(PIXEL) GET_CHANNEL_SIMPLE(RGB565,_RED,PIXEL)
+#define RGB565_GET_GREEN(PIXEL) \
+	GET_CHANNEL(PIXEL[0],3,5) \
+	| (GET_CHANNEL(PIXEL[1],3,0) << 3)
+#define RGB565_GET_BLUE(PIXEL) GET_CHANNEL_SIMPLE(RGB565,_BLUE,PIXEL)
+
+/*****************
+ * XRGB555X or ARGB555X 
+ *****************/
+
+#define XRGB555X_SIZE 2
+
+#define XRGB555X_RED_BYTE 0
+#define XRGB555X_RED_SIZE 5
+#define XRGB555X_RED_POS 2
+
+#define XRGB555X_GREEN_SIZE 5
+
+#define XRGB555X_BLUE_BYTE 1
+#define XRGB555X_BLUE_SIZE 5
+#define XRGB555X_BLUE_POS 0
+
+#define XRGB555X_GET_RED(PIXEL) GET_CHANNEL_SIMPLE(XRGB555X,_RED,PIXEL)
+#define XRGB555X_GET_GREEN(PIXEL) \
+	(GET_CHANNEL(PIXEL[0],2,0) << 3) \
+	| GET_CHANNEL(PIXEL[1],3,5)
+#define XRGB555X_GET_BLUE(PIXEL) GET_CHANNEL_SIMPLE(XRGB555X,_BLUE,PIXEL)
+
+/*****************
+ * RGB565X 
+ *****************/
+
+#define RGB565X_SIZE 2
+
+#define RGB565X_RED_BYTE 0
+#define RGB565X_RED_SIZE 5
+#define RGB565X_RED_POS 3
+
+#define RGB565X_GREEN_SIZE 6
+
+#define RGB565X_BLUE_BYTE 1
+#define RGB565X_BLUE_SIZE 5
+#define RGB565X_BLUE_POS 0
+
+#define RGB565X_GET_RED(PIXEL) GET_CHANNEL_SIMPLE(RGB565X,_RED,PIXEL)
+#define RGB565X_GET_GREEN(PIXEL) \
+	(GET_CHANNEL(PIXEL[0],3,0) << 3) \
+	| GET_CHANNEL(PIXEL[1],3,5)
+#define RGB565X_GET_BLUE(PIXEL) GET_CHANNEL_SIMPLE(RGB565X,_BLUE,PIXEL)
+
+/*****************
+ * BGR24
+ *****************/
+
+#define BGR24_SIZE 3
+#define BGR24_RED_BYTE 2
+#define BGR24_RED_SIZE 8
+#define BGR24_RED_POS 0
+
+#define BGR24_GREEN_BYTE 1
+#define BGR24_GREEN_SIZE 8
+#define BGR24_GREEN_POS 0
+
+#define BGR24_BLUE_BYTE 0
+#define BGR24_BLUE_SIZE 8
+#define BGR24_BLUE_POS 0
+
+#define BGR24_GET_RED(PIXEL) GET_CHANNEL_SIMPLE(BGR24,_RED,PIXEL)
+#define BGR24_GET_GREEN(PIXEL) GET_CHANNEL_SIMPLE(BGR24,_GREEN,PIXEL)
+#define BGR24_GET_BLUE(PIXEL) GET_CHANNEL_SIMPLE(BGR24,_BLUE,PIXEL)
+
+/*****************
+ * RGB24
+ *****************/
+
+#define RGB24_SIZE 3
+#define RGB24_RED_BYTE 0
+#define RGB24_RED_SIZE 8
+#define RGB24_RED_POS 0
+
+#define RGB24_GREEN_BYTE 1
+#define RGB24_GREEN_SIZE 8
+#define RGB24_GREEN_POS 0
+
+#define RGB24_BLUE_BYTE 2
+#define RGB24_BLUE_SIZE 8
+#define RGB24_BLUE_POS 0
+
+#define RGB24_GET_RED(PIXEL) GET_CHANNEL_SIMPLE(RGB24,_RED,PIXEL)
+#define RGB24_GET_GREEN(PIXEL) GET_CHANNEL_SIMPLE(RGB24,_GREEN,PIXEL)
+#define RGB24_GET_BLUE(PIXEL) GET_CHANNEL_SIMPLE(RGB24,_BLUE,PIXEL)
+
+/*****************
+ * BGR666
+ *****************/
+// |byte 0|byte 1|byte 2|
+// |bbbbbbgg|ggggrrrr|rr______|
+
+#define BGR666_SIZE 3
+
+#define BGR666_RED_SIZE 6
+#define BGR666_GREEN_SIZE 6
+
+#define BGR666_BLUE_BYTE 0
+#define BGR666_BLUE_SIZE 6
+#define BGR666_BLUE_POS 2
+
+#define BGR666_GET_RED(PIXEL) \
+	(GET_CHANNEL(PIXEL[1],4,0) << 2) \
+	| GET_CHANNEL(PIXEL[2],2,6)
+#define BGR666_GET_GREEN(PIXEL) \
+	(GET_CHANNEL(PIXEL[0],2,0) << 4) \
+	| GET_CHANNEL(PIXEL[1],4,4)
+#define BGR666_GET_BLUE(PIXEL) GET_CHANNEL_SIMPLE(BGR666,_BLUE,PIXEL)
+
+/*****************
+ * ABGR32 or XBGR32
+ *****************/
+// |byte 0|byte 1|byte 2|byte3|
+// |b|g|r|x|
+
+#define XBGR32_SIZE 4
+
+#define XBGR32_RED_BYTE 2
+#define XBGR32_RED_SIZE 8
+#define XBGR32_RED_POS 0
+
+#define XBGR32_GREEN_BYTE 1
+#define XBGR32_GREEN_SIZE 8
+#define XBGR32_GREEN_POS 0
+
+#define XBGR32_BLUE_BYTE 0
+#define XBGR32_BLUE_SIZE 8
+#define XBGR32_BLUE_POS 0
+
+#define XBGR32_GET_RED(PIXEL) GET_CHANNEL_SIMPLE(XBGR32,_RED,PIXEL)
+#define XBGR32_GET_GREEN(PIXEL) GET_CHANNEL_SIMPLE(XBGR32,_GREEN,PIXEL)
+#define XBGR32_GET_BLUE(PIXEL) GET_CHANNEL_SIMPLE(XBGR32,_BLUE,PIXEL)
+
+/*****************
+ * ARGB32 or XRGB32
+ *****************/
+// |byte 0|byte 1|byte 2|byte3|
+// |x|b|g|r|
+
+#define XRGB32_SIZE 4
+
+#define XRGB32_RED_BYTE 1
+#define XRGB32_RED_SIZE 8
+#define XRGB32_RED_POS 0
+
+#define XRGB32_GREEN_BYTE 2
+#define XRGB32_GREEN_SIZE 8
+#define XRGB32_GREEN_POS 0
+
+#define XRGB32_BLUE_BYTE 3
+#define XRGB32_BLUE_SIZE 8
+#define XRGB32_BLUE_POS 0
+
+#define XRGB32_GET_RED(PIXEL) GET_CHANNEL_SIMPLE(XRGB32,_RED,PIXEL)
+#define XRGB32_GET_GREEN(PIXEL) GET_CHANNEL_SIMPLE(XRGB32,_GREEN,PIXEL)
+#define XRGB32_GET_BLUE(PIXEL) GET_CHANNEL_SIMPLE(XRGB32,_BLUE,PIXEL)
+
+/*****************
+ * Utils
+ *****************/
+
+#define CAT(a, ...) a ## __VA_ARGS__
+#define CAT2(a,b, ...) a ## b ## __VA_ARGS__
+#define CAT3(a,b,c ...) a ## b ## c ## __VA_ARGS__
+
+#define GET_SIZE(FORMAT) \
+	CAT(FORMAT,_SIZE)
 
 #define GET_RED(FORMAT, PIXEL) \
 	RANGE_FROM_TO( \
@@ -57,6 +295,15 @@
 	)
 
 
+#define CONVERT_PIXEL(FORMAT,SRC_BUFFER,DST_BUFFER) { \
+	for( uint i=0; i< size; i++ ) { \
+		const byte_t* current_pixel = &SRC_BUFFER[i*GET_SIZE(FORMAT)]; \
+		DST_BUFFER[i*3+0] = GET_RED(FORMAT,current_pixel); \
+		DST_BUFFER[i*3+1] = GET_GREEN(FORMAT,current_pixel); \
+		DST_BUFFER[i*3+2] = GET_BLUE(FORMAT,current_pixel); \
+	} \
+}
+
 ret_t image_save_ppm(
 		const char* comment,
 		const void* buffer,
@@ -65,7 +312,24 @@ ret_t image_save_ppm(
 		const char* filename
 )
 {
-	if( format.pixelformat != V4L2_PIX_FMT_RGB332 ) {
+	if( !(
+			format.pixelformat == V4L2_PIX_FMT_RGB332
+			|| format.pixelformat == V4L2_PIX_FMT_ARGB444
+			|| format.pixelformat == V4L2_PIX_FMT_XRGB444
+			|| format.pixelformat == V4L2_PIX_FMT_ARGB555
+			|| format.pixelformat == V4L2_PIX_FMT_XRGB555
+			|| format.pixelformat == V4L2_PIX_FMT_RGB565
+			|| format.pixelformat == V4L2_PIX_FMT_ARGB555X
+			|| format.pixelformat == V4L2_PIX_FMT_XRGB555X
+			|| format.pixelformat == V4L2_PIX_FMT_RGB565X
+			|| format.pixelformat == V4L2_PIX_FMT_BGR24
+			|| format.pixelformat == V4L2_PIX_FMT_RGB24
+			|| format.pixelformat == V4L2_PIX_FMT_BGR666
+			|| format.pixelformat == V4L2_PIX_FMT_ABGR32
+			|| format.pixelformat == V4L2_PIX_FMT_XBGR32
+			|| format.pixelformat == V4L2_PIX_FMT_ARGB32
+			|| format.pixelformat == V4L2_PIX_FMT_XRGB32
+	) ) {
 		ERROR( "format not supported" );
 		return RET_FAILURE;
 	}
@@ -84,17 +348,55 @@ ret_t image_save_ppm(
 		);
 		byte_t write_buffer[format.width * format.height * 3];
 		const byte_t* input_buffer = CAST_TO_BYTE_PTR( buffer );
-		for( uint i=0; i< size; i++ ) {
-			const RGB332_TYPE current_pixel = input_buffer[i*sizeof(RGB332_TYPE)];
-			const byte_t red =
-				GET_RED( RGB332, current_pixel );
-			const byte_t green =
-				GET_GREEN( RGB332, current_pixel );
-			const byte_t blue =
-				GET_BLUE( RGB332, current_pixel );
-			write_buffer[i*3+0] = red;
-			write_buffer[i*3+1] = green;
-			write_buffer[i*3+2] = blue;
+		if( format.pixelformat == V4L2_PIX_FMT_RGB332 ) {
+			CONVERT_PIXEL(RGB332,input_buffer,write_buffer)
+		}
+		else if(
+				format.pixelformat == V4L2_PIX_FMT_ARGB444
+				|| format.pixelformat == V4L2_PIX_FMT_XRGB444
+		) {
+			CONVERT_PIXEL(XRGB444,input_buffer,write_buffer)
+		}
+		else if(
+				format.pixelformat == V4L2_PIX_FMT_ARGB555
+				|| format.pixelformat == V4L2_PIX_FMT_XRGB555
+		) {
+			CONVERT_PIXEL(XRGB555,input_buffer,write_buffer)
+		}
+		else if( format.pixelformat == V4L2_PIX_FMT_RGB565
+		) {
+			CONVERT_PIXEL(RGB565,input_buffer,write_buffer)
+		}
+		else if(
+				format.pixelformat == V4L2_PIX_FMT_ARGB555X
+				|| format.pixelformat == V4L2_PIX_FMT_XRGB555X
+		) {
+			CONVERT_PIXEL(XRGB555X,input_buffer,write_buffer)
+		}
+		else if( format.pixelformat == V4L2_PIX_FMT_RGB565X
+		) {
+			CONVERT_PIXEL(RGB565X,input_buffer,write_buffer)
+		}
+		else if( format.pixelformat == V4L2_PIX_FMT_BGR24 ) {
+			CONVERT_PIXEL(BGR24,input_buffer,write_buffer)
+		}
+		else if( format.pixelformat == V4L2_PIX_FMT_RGB24 ) {
+			CONVERT_PIXEL(RGB24,input_buffer,write_buffer)
+		}
+		else if( format.pixelformat == V4L2_PIX_FMT_BGR666 ) {
+			CONVERT_PIXEL(BGR666,input_buffer,write_buffer)
+		}
+		else if(
+				format.pixelformat == V4L2_PIX_FMT_ABGR32
+				|| format.pixelformat == V4L2_PIX_FMT_XBGR32
+		) {
+			CONVERT_PIXEL(XBGR32,input_buffer,write_buffer)
+		}
+		else if(
+				format.pixelformat == V4L2_PIX_FMT_ARGB32
+				|| format.pixelformat == V4L2_PIX_FMT_XRGB32
+		) {
+			CONVERT_PIXEL(XRGB32,input_buffer,write_buffer)
 		}
 		for( uint i=0; i< format.width * format.height; i++ ) {
 			size_t ret = fwrite( &write_buffer[i*3], 1, 3, fd );
--- tests/test_image.c
+++ tests/test_image.c
@@ -2,6 +2,7 @@
 #include "global.h"
 
 #include <check.h>
+#include <linux/videodev2.h>
 
 
 #define CHECK_IMAGE_SUCCESS( CALL ) \
@@ -18,7 +19,7 @@
  * test case
 ***********************/
 
-START_TEST(test_image_save_ppm) {
+START_TEST(test_image_save_ppm_RGB332) {
 	const char* filename = "/tmp/test.ppm";
 	const byte_t RED = 7 << 5; // 0b11100000
 	const byte_t GREEN = 7 << 2; // 0b00011100
@@ -28,7 +29,7 @@ START_TEST(test_image_save_ppm) {
 		.height = 2,
 		.pixelformat = V4L2_PIX_FMT_RGB332,
 	};
-	const unsigned char buffer[] = {
+	const byte_t buffer[] = {
 		RED, GREEN, BLUE,
 		RED|GREEN, RED|GREEN|BLUE, 0
 	};
@@ -36,6 +37,429 @@ START_TEST(test_image_save_ppm) {
 			"hello world",
 			buffer,
 			6,
+			format,
+			filename
+	);
+	FILE* fp = fopen( filename, "r" );
+	ck_assert_msg( fp != NULL, "file does not exist" );
+	fseek( fp, 0, SEEK_END );
+	int file_size = ftell( fp );
+	ck_assert_msg( file_size > 0, "file has size 0" );
+	fseek( fp, 0, SEEK_SET );
+	byte_t expected[] =
+		"P6\n"
+		"#hello world\n"
+		"3 2 255\n"
+		"\xff\x0\x0" "\x0\xff\x0" "\x0\x0\xff"
+		"\xff\xff\x0" "\xff\xff\xff" "\x0\x0\x0"
+	;
+	byte_t read_buffer[file_size];
+	fread( read_buffer, 1, file_size, fp );
+	ck_assert_msg( !memcmp( read_buffer, expected, file_size ),
+			"file content not matching"
+	);
+	fclose( fp );
+}
+END_TEST
+
+START_TEST(test_image_save_ppm_XRGB444) {
+	const char* filename = "/tmp/test_xrgb444.ppm";
+	const img_format_t format = {
+		.width = 3,
+		.height = 2,
+		.pixelformat = V4L2_PIX_FMT_XRGB444,
+	};
+	const byte_t HIGH = 0xF0;
+	const byte_t LOW = 0x0F;
+	// | byte0 | byte1 |
+	// | g, b  | _ r   |
+	const byte_t buffer[] = {
+		0, LOW, /**/ HIGH, 0, /**/ LOW, 0,
+		HIGH, LOW, /**/ HIGH|LOW, LOW, /**/ 0, 0
+	};
+	image_save_ppm(
+			"hello world",
+			buffer,
+			6*2,
+			format,
+			filename
+	);
+	FILE* fp = fopen( filename, "r" );
+	ck_assert_msg( fp != NULL, "file does not exist" );
+	fseek( fp, 0, SEEK_END );
+	int file_size = ftell( fp );
+	ck_assert_msg( file_size > 0, "file has size 0" );
+	ck_assert_int_lt( file_size, 100 );
+	fseek( fp, 0, SEEK_SET );
+	byte_t expected[] =
+		"P6\n"
+		"#hello world\n"
+		"3 2 255\n"
+		"\xff\x0\x0" "\x0\xff\x0" "\x0\x0\xff"
+		"\xff\xff\x0" "\xff\xff\xff" "\x0\x0\x0"
+	;
+	byte_t read_buffer[file_size];
+	fread( read_buffer, 1, file_size, fp );
+	ck_assert_msg( !memcmp( read_buffer, expected, file_size ),
+			"file content not matching"
+	);
+	fclose( fp );
+}
+END_TEST
+
+START_TEST(test_image_save_ppm_XRGB555) {
+	const char* filename = "/tmp/test_xrgb555.ppm";
+	const img_format_t format = {
+		.width = 3,
+		.height = 2,
+		.pixelformat = V4L2_PIX_FMT_XRGB555,
+	};
+	// | byte0 | byte1 |
+	// | gggbbbbb  | _rrrrrgg   |
+	const byte_t buffer[] = {
+		0, 0b01111100, /**/ 0b11100000, 0b00000011, /**/ 0b00011111, 0,
+		0b11100000, 0b01111111, /**/ 0xFF, 0b01111111, /**/ 0, 0
+	};
+	image_save_ppm(
+			"hello world",
+			buffer,
+			6*2,
+			format,
+			filename
+	);
+	FILE* fp = fopen( filename, "r" );
+	ck_assert_msg( fp != NULL, "file does not exist" );
+	fseek( fp, 0, SEEK_END );
+	int file_size = ftell( fp );
+	ck_assert_msg( file_size > 0, "file has size 0" );
+	ck_assert_int_lt( file_size, 100 );
+	fseek( fp, 0, SEEK_SET );
+	byte_t expected[] =
+		"P6\n"
+		"#hello world\n"
+		"3 2 255\n"
+		"\xff\x0\x0" "\x0\xff\x0" "\x0\x0\xff"
+		"\xff\xff\x0" "\xff\xff\xff" "\x0\x0\x0"
+	;
+	byte_t read_buffer[file_size];
+	fread( read_buffer, 1, file_size, fp );
+	ck_assert_msg( !memcmp( read_buffer, expected, file_size ),
+			"file content not matching"
+	);
+	fclose( fp );
+}
+END_TEST
+
+START_TEST(test_image_save_ppm_RGB565) {
+	const char* filename = "/tmp/test_rgb565.ppm";
+	const img_format_t format = {
+		.width = 3,
+		.height = 2,
+		.pixelformat = V4L2_PIX_FMT_RGB565,
+	};
+	// | byte0 | byte1 |
+	// | gggbbbbb | rrrrrggg |
+	const byte_t buffer[] = {
+		0, 0b11111000, /**/ 0b11100000, 0b00000111, /**/ 0b00011111, 0,
+		0b11100000, 0b11111111, /**/ 0xFF, 0xFF, /**/ 0, 0
+	};
+	image_save_ppm(
+			"hello world",
+			buffer,
+			6*2,
+			format,
+			filename
+	);
+	FILE* fp = fopen( filename, "r" );
+	ck_assert_msg( fp != NULL, "file does not exist" );
+	fseek( fp, 0, SEEK_END );
+	int file_size = ftell( fp );
+	ck_assert_msg( file_size > 0, "file has size 0" );
+	ck_assert_int_lt( file_size, 100 );
+	fseek( fp, 0, SEEK_SET );
+	byte_t expected[] =
+		"P6\n"
+		"#hello world\n"
+		"3 2 255\n"
+		"\xff\x0\x0" "\x0\xff\x0" "\x0\x0\xff"
+		"\xff\xff\x0" "\xff\xff\xff" "\x0\x0\x0"
+	;
+	byte_t read_buffer[file_size];
+	fread( read_buffer, 1, file_size, fp );
+	ck_assert_msg( !memcmp( read_buffer, expected, file_size ),
+			"file content not matching"
+	);
+	fclose( fp );
+}
+END_TEST
+
+START_TEST(test_image_save_ppm_XRGB555X) {
+	const char* filename = "/tmp/test_XRGB555X.ppm";
+	const img_format_t format = {
+		.width = 3,
+		.height = 2,
+		.pixelformat = V4L2_PIX_FMT_XRGB555X,
+	};
+	// | byte0 | byte1 |
+	// | _rrrrrgg | gggbbbbb |
+	const byte_t buffer[] = {
+		0b01111100, 0, /**/ 0b000000011, 0b11100000, /**/ 0, 0b00011111,
+		0b01111111, 0b11100000, /**/ 0xFF, 0xFF, /**/ 0, 0
+	};
+	image_save_ppm(
+			"hello world",
+			buffer,
+			6*2,
+			format,
+			filename
+	);
+	FILE* fp = fopen( filename, "r" );
+	ck_assert_msg( fp != NULL, "file does not exist" );
+	fseek( fp, 0, SEEK_END );
+	int file_size = ftell( fp );
+	ck_assert_msg( file_size > 0, "file has size 0" );
+	ck_assert_int_lt( file_size, 100 );
+	fseek( fp, 0, SEEK_SET );
+	byte_t expected[] =
+		"P6\n"
+		"#hello world\n"
+		"3 2 255\n"
+		"\xff\x0\x0" "\x0\xff\x0" "\x0\x0\xff"
+		"\xff\xff\x0" "\xff\xff\xff" "\x0\x0\x0"
+	;
+	byte_t read_buffer[file_size];
+	fread( read_buffer, 1, file_size, fp );
+	ck_assert_msg( !memcmp( read_buffer, expected, file_size ),
+			"file content not matching"
+	);
+	fclose( fp );
+}
+END_TEST
+
+START_TEST(test_image_save_ppm_RGB565X) {
+	const char* filename = "/tmp/test_RGB565X.ppm";
+	const img_format_t format = {
+		.width = 3,
+		.height = 2,
+		.pixelformat = V4L2_PIX_FMT_RGB565X,
+	};
+	// | byte0 | byte1 |
+	// | rrrrrggg | gggbbbbb |
+	const byte_t buffer[] = {
+		0b11111000, 0, /**/ 0b000000111, 0b11100000, /**/ 0, 0b00011111,
+		0b11111111, 0b11100000, /**/ 0xFF, 0xFF, /**/ 0, 0
+	};
+	image_save_ppm(
+			"hello world",
+			buffer,
+			6*2,
+			format,
+			filename
+	);
+	FILE* fp = fopen( filename, "r" );
+	ck_assert_msg( fp != NULL, "file does not exist" );
+	fseek( fp, 0, SEEK_END );
+	int file_size = ftell( fp );
+	ck_assert_msg( file_size > 0, "file has size 0" );
+	ck_assert_int_lt( file_size, 100 );
+	fseek( fp, 0, SEEK_SET );
+	byte_t expected[] =
+		"P6\n"
+		"#hello world\n"
+		"3 2 255\n"
+		"\xff\x0\x0" "\x0\xff\x0" "\x0\x0\xff"
+		"\xff\xff\x0" "\xff\xff\xff" "\x0\x0\x0"
+	;
+	byte_t read_buffer[file_size];
+	fread( read_buffer, 1, file_size, fp );
+	ck_assert_msg( !memcmp( read_buffer, expected, file_size ),
+			"file content not matching"
+	);
+	fclose( fp );
+}
+END_TEST
+
+START_TEST(test_image_save_ppm_BGR24) {
+	const char* filename = "/tmp/test_BGR24.ppm";
+	const img_format_t format = {
+		.width = 3,
+		.height = 2,
+		.pixelformat = V4L2_PIX_FMT_BGR24,
+	};
+	const byte_t buffer[] = {
+		0, 0, 0xFF, /**/ 0, 0xFF, 0, /**/ 0xFF, 0, 0, 
+		0, 0xFF, 0xFF, /**/ 0xFF, 0xFF, 0xFF, /**/ 0, 0, 0
+	};
+	image_save_ppm(
+			"hello world",
+			buffer,
+			6*3,
+			format,
+			filename
+	);
+	FILE* fp = fopen( filename, "r" );
+	ck_assert_msg( fp != NULL, "file does not exist" );
+	fseek( fp, 0, SEEK_END );
+	int file_size = ftell( fp );
+	ck_assert_msg( file_size > 0, "file has size 0" );
+	fseek( fp, 0, SEEK_SET );
+	byte_t expected[] =
+		"P6\n"
+		"#hello world\n"
+		"3 2 255\n"
+		"\xff\x0\x0" "\x0\xff\x0" "\x0\x0\xff"
+		"\xff\xff\x0" "\xff\xff\xff" "\x0\x0\x0"
+	;
+	byte_t read_buffer[file_size];
+	fread( read_buffer, 1, file_size, fp );
+	ck_assert_msg( !memcmp( read_buffer, expected, file_size ),
+			"file content not matching"
+	);
+	fclose( fp );
+}
+END_TEST
+
+START_TEST(test_image_save_ppm_RGB24) {
+	const char* filename = "/tmp/test_rgb24.ppm";
+	const img_format_t format = {
+		.width = 3,
+		.height = 2,
+		.pixelformat = V4L2_PIX_FMT_RGB24,
+	};
+	const byte_t buffer[] = {
+		0xFF, 0, 0, /**/ 0, 0xFF, 0, /**/ 0, 0, 0xFF, 
+		0xFF, 0xFF, 0, /**/ 0xFF, 0xFF, 0xFF, /**/ 0, 0, 0
+	};
+	image_save_ppm(
+			"hello world",
+			buffer,
+			6*3,
+			format,
+			filename
+	);
+	FILE* fp = fopen( filename, "r" );
+	ck_assert_msg( fp != NULL, "file does not exist" );
+	fseek( fp, 0, SEEK_END );
+	int file_size = ftell( fp );
+	ck_assert_msg( file_size > 0, "file has size 0" );
+	fseek( fp, 0, SEEK_SET );
+	byte_t expected[] =
+		"P6\n"
+		"#hello world\n"
+		"3 2 255\n"
+		"\xff\x0\x0" "\x0\xff\x0" "\x0\x0\xff"
+		"\xff\xff\x0" "\xff\xff\xff" "\x0\x0\x0"
+	;
+	byte_t read_buffer[file_size];
+	fread( read_buffer, 1, file_size, fp );
+	ck_assert_msg( !memcmp( read_buffer, expected, file_size ),
+			"file content not matching"
+	);
+	fclose( fp );
+}
+END_TEST
+
+START_TEST(test_image_save_ppm_BGR666) {
+	const char* filename = "/tmp/test_BGR666.ppm";
+	const img_format_t format = {
+		.width = 3,
+		.height = 2,
+		.pixelformat = V4L2_PIX_FMT_BGR666,
+	};
+	// |byte 0|byte 1|byte 2|
+	// |bbbbbbgg|ggggrrrr|rr______|
+	const byte_t buffer[] = {
+		0, 0b00001111, 0b11000000, /**/ 0b00000011, 0b11110000, 0, /**/ 0b11111100, 0, 0, 
+		0b00000011, 0xFF, 0b11000000, /**/ 0xFF, 0xFF, 0xFF, /**/ 0, 0, 0
+	};
+	image_save_ppm(
+			"hello world",
+			buffer,
+			6*3,
+			format,
+			filename
+	);
+	FILE* fp = fopen( filename, "r" );
+	ck_assert_msg( fp != NULL, "file does not exist" );
+	fseek( fp, 0, SEEK_END );
+	int file_size = ftell( fp );
+	ck_assert_msg( file_size > 0, "file has size 0" );
+	fseek( fp, 0, SEEK_SET );
+	byte_t expected[] =
+		"P6\n"
+		"#hello world\n"
+		"3 2 255\n"
+		"\xff\x0\x0" "\x0\xff\x0" "\x0\x0\xff"
+		"\xff\xff\x0" "\xff\xff\xff" "\x0\x0\x0"
+	;
+	byte_t read_buffer[file_size];
+	fread( read_buffer, 1, file_size, fp );
+	ck_assert_msg( !memcmp( read_buffer, expected, file_size ),
+			"file content not matching"
+	);
+	fclose( fp );
+}
+END_TEST
+
+START_TEST(test_image_save_ppm_XBGR32) {
+	const char* filename = "/tmp/test_XBGR32.ppm";
+	const img_format_t format = {
+		.width = 3,
+		.height = 2,
+		.pixelformat = V4L2_PIX_FMT_XBGR32,
+	};
+	// |byte 0|byte 1|byte 2|byte3|
+	// |b|g|r|x|
+	const byte_t buffer[] = {
+		0,0,0xFF,0, /**/ 0,0xFF,0,0, /**/ 0xFF,0,0,0,
+		0,0xFF,0xFF,0, /**/ 0xFF, 0xFF, 0xFF, 0, /**/ 0, 0, 0, 0
+	};
+	image_save_ppm(
+			"hello world",
+			buffer,
+			6*3,
+			format,
+			filename
+	);
+	FILE* fp = fopen( filename, "r" );
+	ck_assert_msg( fp != NULL, "file does not exist" );
+	fseek( fp, 0, SEEK_END );
+	int file_size = ftell( fp );
+	ck_assert_msg( file_size > 0, "file has size 0" );
+	fseek( fp, 0, SEEK_SET );
+	byte_t expected[] =
+		"P6\n"
+		"#hello world\n"
+		"3 2 255\n"
+		"\xff\x0\x0" "\x0\xff\x0" "\x0\x0\xff"
+		"\xff\xff\x0" "\xff\xff\xff" "\x0\x0\x0"
+	;
+	byte_t read_buffer[file_size];
+	fread( read_buffer, 1, file_size, fp );
+	ck_assert_msg( !memcmp( read_buffer, expected, file_size ),
+			"file content not matching"
+	);
+	fclose( fp );
+}
+END_TEST
+
+START_TEST(test_image_save_ppm_XRGB32) {
+	const char* filename = "/tmp/test_XRGB32.ppm";
+	const img_format_t format = {
+		.width = 3,
+		.height = 2,
+		.pixelformat = V4L2_PIX_FMT_XRGB32,
+	};
+	// |byte 0|byte 1|byte 2|byte3|
+	// |x|r|g|b|
+	const byte_t buffer[] = {
+		0,0xFF,0,0, /**/ 0,0,0xFF,0, /**/ 0,0,0,0xFF,
+		0,0xFF,0xFF,0, /**/ 0,0xFF,0xFF,0xFF, /**/ 0, 0, 0, 0
+	};
+	image_save_ppm(
+			"hello world",
+			buffer,
+			6*3,
 			format,
 			filename
 	);
@@ -69,7 +493,17 @@ Suite* image_suite() {
 	Suite* suite = suite_create("image");
 	{
 		TCase* test_case = tcase_create("save");
-		tcase_add_test(test_case, test_image_save_ppm);
+		tcase_add_test(test_case, test_image_save_ppm_RGB332);
+		tcase_add_test(test_case, test_image_save_ppm_XRGB444);
+		tcase_add_test(test_case, test_image_save_ppm_XRGB555);
+		tcase_add_test(test_case, test_image_save_ppm_RGB565);
+		tcase_add_test(test_case, test_image_save_ppm_XRGB555X);
+		tcase_add_test(test_case, test_image_save_ppm_RGB565X);
+		tcase_add_test(test_case, test_image_save_ppm_BGR24);
+		tcase_add_test(test_case, test_image_save_ppm_RGB24);
+		tcase_add_test(test_case, test_image_save_ppm_BGR666);
+		tcase_add_test(test_case, test_image_save_ppm_XBGR32);
+		tcase_add_test(test_case, test_image_save_ppm_XRGB32);
 		suite_add_tcase(suite, test_case);
 	}
 	return suite;
